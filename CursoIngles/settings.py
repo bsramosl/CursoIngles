61d031685139409567539908c75dbdee0a71fbd4
Lopez <steeven10f@gmail.com>
2023-04-03 12:26:45 -0500

    11

--- CursoIngles/settings.py
+++ CursoIngles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9f8^^^zwx=su03^r54em!7uskpa#w9vbje3r))2bl21q=+555e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -125,7 +125,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
