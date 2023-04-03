d22f4dba36e7913fbdf137785c50f6a4bd8d2dd3
Lopez <steeven10f@gmail.com>
2023-04-03 11:21:27 -0500

    rewail

--- CursoIngles/settings.py
+++ CursoIngles/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9f8^^^zwx=su03^r54em!7uskpa#w9vbje3r))2bl21q=+555e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["bramosl.pythonanywhere.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
